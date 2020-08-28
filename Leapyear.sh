223b01ee5b8d55c5506c4175252f5f022deead9c
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-28 16:15:20 +0530

    ADDED:To check whether Year is Leap Year or not.

---
+++ Leapyear.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+read -p " Enter a Year  To Check = "  year
+
+        if (( ($year % 4==0) && ($year %400==0 || $year %100!=0) ))
+        then
+                echo " $year is a leap year "
+        else
+                echo " $year is not a leap year "
+        fi
+
